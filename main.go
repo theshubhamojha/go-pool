fd346bbf3ce8e9b0ea0c3cb04413133d186a5f72
Shubham Ojha <shubham.ojha@NTPL-ITMACLAP0421.local>
2022-12-08 23:20:48 +0530

    initial commit to write the basic code

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type poolService struct {
+	limit     int
+	processor func(interface{}) interface{}
+	counter   chan bool
+}
+
+type Pool interface {
+	Process(ctx context.Context, payload interface{})
+}
+
+func NewGoroutinePool(maxRoutines int, process func(interface{}) interface{}) Pool {
+	ch := make(chan bool, maxRoutines)
+
+	return &poolService{
+		limit:     maxRoutines,
+		processor: process,
+		counter:   ch,
+	}
+}
+
+func (s *poolService) executeFunction(payload interface{}, taskId int) {
+	now := time.Now()
+	s.processor(payload)
+	fmt.Printf("program completed, time taken %v, taskId: %d \n", time.Now().Sub(now).Seconds(), taskId)
+	<-s.counter
+}
+
+func (s *poolService) Process(ctx context.Context, payload interface{}) {
+	taskId := rand.Intn(500)
+	now := time.Now()
+
+	fmt.Printf("program waiting for execution, id: %d \n", taskId)
+	s.counter <- true
+	fmt.Printf("program assigned go routine for execution: %d, wait_time: %v \n", taskId, time.Now().Sub(now).Seconds())
+
+	go s.executeFunction(payload, taskId)
+}
+
+// driver code ahead
+// mocking a sample sync function
+func sampleFunction(waitTime interface{}) (result interface{}) {
+	switch duration := waitTime.(type) {
+	case int:
+		time.Sleep(time.Duration(duration) * time.Second)
+	default:
+		fmt.Println("error typecasting waitTime variable to integer")
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	ctx := context.Background()
+
+	// specify total number of routines and the sync function which we want to execute
+	p := NewGoroutinePool(3, sampleFunction)
+
+	p.Process(ctx, 8)
+	p.Process(ctx, 3)
+	p.Process(ctx, 7)
+	p.Process(ctx, 4)
+	p.Process(ctx, 2)
+	p.Process(ctx, 3)
+	p.Process(ctx, 5)
+	p.Process(ctx, 6)
+}
