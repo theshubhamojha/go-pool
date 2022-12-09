f188c51c58cfb8daedcfa54e9d5e7a26c324a594
ujjawal-khare <66246543+ujjawal-khare@users.noreply.github.com>
2022-12-09 17:42:03 +0530

    Generic argument (#1)
    * generic function
    
    * generic function
    
    * --wip-- [skip ci]
    
    * .idea files deleted
    
    Co-authored-by: Ujjawal Khare <ujjawal.khare@D11-Ujjawal.Khare>

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ import (
 
 type poolService struct {
 	limit     int
-	processor func(interface{}) interface{}
+	processor func(...interface{}) interface{}
 	counter   chan bool
 }
 
 type Pool interface {
-	Process(ctx context.Context, payload interface{})
+	Process(ctx context.Context, payload ...interface{})
 }
 
-func NewGoroutinePool(maxRoutines int, process func(interface{}) interface{}) Pool {
+func NewGoroutinePool(maxRoutines int, process func(...interface{}) interface{}) Pool {
 	ch := make(chan bool, maxRoutines)
 
 	return &poolService{
@@ -27,14 +27,14 @@ func NewGoroutinePool(maxRoutines int, process func(interface{}) interface{}) Po
 	}
 }
 
-func (s *poolService) executeFunction(payload interface{}, taskId int) {
+func (s *poolService) executeFunction(taskId int, payload ...interface{}) {
 	now := time.Now()
-	s.processor(payload)
+	s.processor(payload...)
 	fmt.Printf("program completed, time taken %v, taskId: %d \n", time.Now().Sub(now).Seconds(), taskId)
 	<-s.counter
 }
 
-func (s *poolService) Process(ctx context.Context, payload interface{}) {
+func (s *poolService) Process(ctx context.Context, payload ...interface{}) {
 	taskId := rand.Intn(500)
 	now := time.Now()
 
@@ -42,14 +42,16 @@ func (s *poolService) Process(ctx context.Context, payload interface{}) {
 	s.counter <- true
 	fmt.Printf("program assigned go routine for execution: %d, wait_time: %v \n", taskId, time.Now().Sub(now).Seconds())
 
-	go s.executeFunction(payload, taskId)
+	go s.executeFunction(taskId, payload...)
 }
 
 // driver code ahead
 // mocking a sample sync function
-func sampleFunction(waitTime interface{}) (result interface{}) {
-	switch duration := waitTime.(type) {
+func sampleFunction(waitTime ...interface{}) (result interface{}) {
+	fmt.Println("wait ", waitTime)
+	switch duration := waitTime[0].(type) {
 	case int:
+		fmt.Println("Will sleep now", waitTime)
 		time.Sleep(time.Duration(duration) * time.Second)
 	default:
 		fmt.Println("error typecasting waitTime variable to integer")
@@ -63,14 +65,14 @@ func main() {
 	ctx := context.Background()
 
 	// specify total number of routines and the sync function which we want to execute
-	p := NewGoroutinePool(3, sampleFunction)
+	p := NewGoroutinePool(1, sampleFunction)
 
-	p.Process(ctx, 8)
-	p.Process(ctx, 3)
-	p.Process(ctx, 7)
-	p.Process(ctx, 4)
-	p.Process(ctx, 2)
-	p.Process(ctx, 3)
-	p.Process(ctx, 5)
-	p.Process(ctx, 6)
+	p.Process(ctx, 8, 2)
+	p.Process(ctx, 3, 1)
+	//p.Process(ctx, 7)
+	//p.Process(ctx, 4)
+	//p.Process(ctx, 2)
+	//p.Process(ctx, 3)
+	//p.Process(ctx, 5)
+	//p.Process(ctx, 6)
 }
